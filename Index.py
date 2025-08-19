39ed5878481433b604f1f8136568fb56f78948f2
kwame-boakye <boakyekelvin306@gmail.com>
2025-08-19 10:26:18 -0500

    Skeleton of project

--- Index.py
+++ Index.py
@@ -1,1 +1,39 @@
-print("Hello world")
+DB = []
+names = []
+grades = []
+category = []
+
+
+# Agrawal
+def categorization(marks):
+    pass
+
+
+# Uzoma
+def userInteraction():
+    pass
+
+
+# Kelvin
+def saveUser(name, grade, category):
+    pass
+
+
+# Agrawal
+def validateGrade():
+    pass
+
+
+# test case needed
+userInteraction()
+
+
+# additional functions
+"""
+def displayUsers():
+    pass
+
+def deleteUsers():
+    pass
+    
+"""
