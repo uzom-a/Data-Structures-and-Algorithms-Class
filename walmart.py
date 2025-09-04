0f5cf825809c98c44c32233897eef6a19d6f24d5
kwame-boakye <boakyekelvin306@gmail.com>
2025-09-04 09:46:04 -0500

    initial commit

---
+++ walmart.py
@@ -0,0 +1,9 @@
+"""
+This is for lab 2
+
+Create a Python program that simulates a shopping experience at Walmart with a
+budget of $100. The program should ensure that users do not exceed their budget,
+account for
+tax, and display a receipt upon checkout.
+
+"""
