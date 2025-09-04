89b42f1b9fd233fc2c6541d32d1ba593b91b767f
Uzoma Stephanie-Portia Okey-Anyanwu <166033308+uzom-a@users.noreply.github.com>
2025-09-04 10:05:50 -0500

    Added function

--- walmart.py
+++ walmart.py
@@ -10,4 +10,16 @@ tax, and display a receipt upon checkout.
 
 def add_product():
     item_name = input("Enter the product's name: ").strip()
+
+    while True:
+            if digit:
+                print("Invalid input. Letter cannot be a price")
+            else:
+                 pass
+
     item_price = int(input("Enter the product's price: "))
+
+
+def calculate_tax(item_price):
+    tax_rate = 10.44 / 100
+    return item_price * tax_rate + item_price
